1214150493243e3ca8284f942103e9d92c19925b
walid-hud <walidthehud@gmail.com>
2026-06-23 16:19:40 +0100

    test(setup): setup mock db
    - Switched to `vitest` over `jest` due to the later not supporting esm out
    of the box
    - some unit test for the user service

--- tests/user.service.test.js
+++ tests/user.service.test.js
@@ -1,14 +1,28 @@
-import * as userService from "../../services/user.service.js";
-describe("user service", () => {
-  test("create user", async () => {
-    const data = {
-      username: "john doe",
-      email: "john@doe.com",
-      role: "user",
-      password: "12345678",
-    };
+import * as userService from "../services/user.service.js";
+import { describe, it, expect, beforeAll, afterEach, afterAll } from "vitest";
+import { connect, closeDatabase, clearDatabase } from "./utils.js";
 
-    const created = await userService.createUser(data);
-    expect(created).toMatchObject(data);
+beforeAll(async () => await connect());
+afterEach(async () => await clearDatabase());
+afterAll(async () => await closeDatabase());
+
+describe("user service tests", () => {
+  const mockUser = {
+    username: "john doe",
+    password_hash: "password",
+    email: "john@doe.com",
+    role: "user",
+  };
+
+  it("create user", async () => {
+    const created = await userService.createUser(mockUser);
+
+    expect(created.id).toBeDefined();
+    expect(created).toMatchObject(mockUser);
+    expect(userService.createUser(mockUser)).rejects.toThrow();
+  });
+  it("throws when trying to create user with existing email", async () => {
+    await userService.createUser(mockUser);
+    expect(userService.createUser(mockUser)).rejects.toThrow();
   });
 });
